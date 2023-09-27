d4b80c875f08db27867082b95851119bd83f2ae2
Charlotte <charlotteleyser@icloud.com>
2023-09-27 13:05:04 +0200

    Add project files.

---
+++ Lektion 0927/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Lektion_0927
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+        }
+    }
+}
